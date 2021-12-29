d93e3a8d5f519b33a4fee0ee4c9e504f46e26494
unknown <pkbarnwal3@gmail.com>
2021-12-28 18:18:32 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  let [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+      showAlert("Dark Mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has been enabled", "success");
+    }
+  };
+
+  return (
+    <>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route exact path="/about">
+              <About showAlert={showAlert} />
+            </Route>
+            <Route exact path="/">
+              <TextForm
+                showAlert={showAlert}
+                heading="Enter the text below to analyze"
+              />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to uppercase", "success");
+  };
+
+  const handleDownClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to lowercase", "success");
+  };
+
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Text cleared", "success");
+  };
+
+  const handleCopyClick = () => {
+    let newText = document.getElementById("myBox");
+    newText.select();
+    navigator.clipboard.writeText(newText.value);
+    props.showAlert("Copied to clipboard", "success");
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Extra spaces removed", "success");
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div className="conatainer">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows={8}
+          />
+        </div>{" "}
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-2"
+          onClick={handleUpClick}
+        >
+          Convert to Uppercase
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-2"
+          onClick={handleDownClick}
+        >
+          Convert to Lowercase
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-danger mx-2 my-2"
+          onClick={handleClearClick}
+        >
+          Clear
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-success mx-2 my-2"
+          onClick={handleCopyClick}
+        >
+          Copy Text
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-success mx-2 my-2"
+          onClick={handleExtraSpaces}
+        >
+          Remove Extra Spaces
+        </button>
+      </div>
+      <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>
+          {
+            text.split(/\s+/).filter((element) => {
+              return element.length !== 0;
+            }).length
+          }{" "}
+          words and {text.length} characters
+        </p>
+        <p>
+          {0.008 *
+            text.split(" ").filter((element) => {
+              return element.length !== 0;
+            }).length}{" "}
+          Minutes read
+        </p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "Nothing to preview"}</p>
+      </div>
+    </>
+  );
+}
